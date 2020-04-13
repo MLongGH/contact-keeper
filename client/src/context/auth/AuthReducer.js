b5812abee7e3a8d98a74852290a889f1c108fb5b
meilinglong <meilingl.usu@gmail.com>
2020-04-13 10:08:50 -0600

    Load User & set token

--- client/src/context/auth/AuthReducer.js
+++ client/src/context/auth/AuthReducer.js
@@ -9,6 +9,13 @@ import {
 
 export default (state, action) => {
   switch(action.type){
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated:true,
+        loading:false,
+        user: action.payload
+      }
     case REGISTER_SUCCESS:
       localStorage.setItem('token',action.payload.token);
       return {
@@ -18,6 +25,7 @@ export default (state, action) => {
         loading:false
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
