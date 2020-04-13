ffc2c85271ee0a6d3689d79b4a7399dad592c1cb
meilinglong <meilingl.usu@gmail.com>
2020-04-13 11:32:46 -0600

    User Login

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -60,7 +60,7 @@ const AuthState = props => {
           type: REGISTER_SUCCESS,
           payload: res.data //token
         });
-        
+
         loadUser();
       }catch(err){
         dispatch({
@@ -70,7 +70,29 @@ const AuthState = props => {
       }
     }
     //Login User
-    const login = () => console.log('login');
+    const login = async formData => {
+      const config = {
+        headers: {
+          'content-type':'application/json'
+        }
+      }
+
+      try {
+        const res = await axios.post('/api/auth',formData,config);
+
+        dispatch({
+          type: LOGIN_SUCCESS,
+          payload: res.data
+        });
+        
+        loadUser();
+      }catch(err){
+        dispatch({
+          type:LOGIN_FAIL,
+          payload: err.response.data.msg
+        })
+      }
+    }
     //Logout
     const logout = () => console.log('logout');
     //Clear Errors
